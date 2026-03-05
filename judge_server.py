e7cb12536e1bb76e7cad31325e7e44fd085a3ef5
ironman127583yr-netizen <ironman127583yr@gmail.com>
2026-03-05 12:57:20 +0530

    Update judge_server.py

--- judge_server.py
+++ judge_server.py
@@ -76,4 +76,4 @@ async def judge(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("judge_server:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
