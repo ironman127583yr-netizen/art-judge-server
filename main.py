e234d2307c3c5fb6d31fac6ad9e891cce5e7bc62
ironman127583yr-netizen <ironman127583yr@gmail.com>
2026-03-19 12:06:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 import asyncio
 
-from worker import judge_worker
 from match import router
+from worker import judge_worker
 
 app = FastAPI()
 
+# register routes
 app.include_router(router)
 
+# start background worker
 @app.on_event("startup")
 async def startup():
     asyncio.create_task(judge_worker())
